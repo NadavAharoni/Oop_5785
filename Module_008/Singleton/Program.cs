71811589c69091aa3e098eeb4b4db6b6cb9e9ba7
Nadav Aharoni <nadav.aharoni@gmail.com>
2025-01-12 19:23:07 +0200

    Added Singleton Project

---
+++ Module_008/Singleton/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Singleton
+{
+    class Page
+    {
+        double width = 10;
+        double height = 20;
+        private Page()
+        {
+            Console.WriteLine("Calling Page() ctor");
+        }
+
+        private static Page? thePage = null;
+        public static Page getThePage()
+        {
+            if (thePage == null)
+            {
+                thePage = new Page();
+            }
+            return thePage;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("In Main()");
+            Page p1 = Page.getThePage();
+            Page p2 = Page.getThePage();
+        }
+    }
+}
